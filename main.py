79f08cd19933f3b79705bb0c8f7d02bea9f7ab6e
vaish-s <72965637+vaish-s@users.noreply.github.com>
2021-10-13 14:04:05 +0530

    Root Squared Mean Error Calculated

--- main.py
+++ main.py
@@ -11,18 +11,19 @@ diabetes = datasets.load_diabetes()
 # dict_keys(['data', 'target', 'frame', 'DESCR', 'feature_names', 'data_filename', 'target_filename', 'data_module'])
 diabetes_X = diabetes.data[:, np.newaxis, 2]
 
-diabetes_X_train = diabetes_X[:-40]
-diabetes_X_test = diabetes_X[-20:]
+diabetes_X_train = diabetes_X[:-50]
+diabetes_X_test = diabetes_X[-10:]
 
-diabetes_y_train = diabetes.target[:-40]
-diabetes_y_test = diabetes.target[-20:]
+diabetes_y_train = diabetes.target[:-50]
+diabetes_y_test = diabetes.target[-10:]
 
 model = linear_model.LinearRegression()
 model.fit(diabetes_X_train, diabetes_y_train)
 
 diabetes_predicted = model.predict(diabetes_X_test)
 
-print("Mean Squarred Error is :", mean_squared_error(diabetes_y_test, diabetes_predicted))
+print("Mean Squared Error is :", mean_squared_error(diabetes_y_test, diabetes_predicted))
+print("Root Mean Squared Error i s:", sqrt(mean_squared_error(diabetes_y_test, diabetes_predicted)))
 print("Weights:", model.coef_)
 print("Intercept:", model.intercept_)
 
